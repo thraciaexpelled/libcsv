b9b81c150a3cabe61aca112c1bb429700e69b3fe
thraciaexpelled <chudaomaa00@gmail.com>
2025-07-18 03:59:21 +0600

    meta: project structure pt 2

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
 include config.mk
 
+.PHONY: tidy clean install
+
+$(BINNAMEINFLAG): $(SRCDIR)/main.c
+	$(CC) $(CCFLAGS) $< -shared -o $@
+	@echo "Run *make tidy* to package this library"
+
+tidy: $(BUILDDIR)
+	python3 ./utils/janitor.py $(BUILDDIR)
+
+clean:
+	rm -f *.o
+	rm -f *$(BINFMT)
+	rm -f $(BINNAME)
+
+install:
+	@echo "Not implemented yet"
--- config.mk
+++ config.mk
@@ -1,29 +1,31 @@
 # Main Directories
 
 ## Source directory
-SRCDIR = src/
+SRCDIR = src
 
 ## Include (header) directory
-INCDIR = $(SRCDIR)headers/
+INCDIR = $(SRCDIR)/headers
 
 # Post Compile Directories
 
 ## Binary name
 BINNAME = libcsv
 
+### Binary format (default: .dylib, MUST change according to your OS)
+BINFMT = .dylib
+
 ### Binary name used in compiler flags
-BINNAMEINFLAG = libcsv$(shell ./makehelpers/friend_of_all_makefiles.sh return_dylib_file_format)
+BINNAMEINFLAG = $(BINNAME)$(BINFMT)
 
 ## Version
-VERSION != ./makehelpers/friend_of_all_makefiles.sh return_version
-MARCH != uname -m
-MOS != ./makehelpers/friend_of_all_makefiles.sh return_enduser_os | tr -d " " | tr "[:upper:]" "[:lower:]"
+VERSION = 0.1.0
+MARCH = $(shell uname -m)
 
 ## Build directory
-BUILDDIR = $(BINNAME)-$(MARCH)-$(VERSION)-$(MOS)/
+BUILDDIR = $(BINNAME)-$(MARCH):$(VERSION)/
 
-# Default Compiler
-CC != ./makehelpers/friend_of_all_makefiles.sh return_optimal_compiler
+# Default Compiler (change to whatever you prefer)
+CC = clang
 
 ## Compiler flags for default compiler
 CCFLAGS = -Wall -Wextra -Wpedantic -O2 -march=native
